b0cfb77c6347f11c3a2057fdca839aabfbc33e63
moisotico <moiso.camposcr@gmail.com>
2020-12-24 16:17:40 -0600

    Working on PRODUCTS.csv filtering

--- integrations/richart_wholesale_club/ingestion.py
+++ integrations/richart_wholesale_club/ingestion.py
@@ -1,31 +1,83 @@
 import numpy as np
 import pandas as pd
 import requests as rq
+import re
 
+# using compile for a regular expression
+TAG_RE = re.compile(r'<[^>]+>')
 
-def process_csv_files():
-    # urls to obtain dataframe
-    products_url = "https://cornershop-scrapers-evaluation.s3.amazonaws.com/public/PRODUCTS.csv"
-    prices_url = "https://cornershop-scrapers-evaluation.s3.amazonaws.com/public/PRICES-STOCK.csv"
-
-    response = rq.get(prices_url)
-    print("Status Code:", response.status_code)
+# function to check urls
+def getResponse(url):
+    response = rq.get(url)
+    print("GET:", url, "Status Code:", response.status_code)
     if response:
-        print('Success!')
+        return 0
     else:
-        print('An error has occurred.')
+        print('An error has occurred with the response:')
         return -1
-    # put the dataframe into PRODUCTS.csv
-    products_df = pd.read_csv(products_url, sep='|')
+
+
+# TODO: refactor
+def checkFiles():
+    try:
+        f = open('./data/PRODUCTS.csv')
+        f = open('./data/PRICES-STOCK.csv')
+        return
+    except FileNotFoundError:
+        print("File not accessible, starting download")
+        products_url = "https://cornershop-scrapers-evaluation.s3.amazonaws.com/public/PRODUCTS.csv"
+        prices_url = "https://cornershop-scrapers-evaluation.s3.amazonaws.com/public/PRICES-STOCK.csv"
+
+        getResponse(products_url)
+        print("Downloading PRODUCTS.csv")
+        products_df = pd.read_csv(products_url)
+        products_df.to_csv('./data/PRODUCTS.csv', header=True)
+
+        getResponse(prices_url)
+        print("Downloading PRICES-STOCK.csv")
+        prices_df = pd.read_csv(prices_url)
+        prices_df.to_csv('./data/PRICES-STOCK.csv', header=True)
+
+
+# Function to remove HTML tags via a regex to match one or more characters
+#  inside a tag "< >""
+def removeHtmlTags(string):
+    string = TAG_RE.sub(' ', string)
+    string = string.replace('\\"', ' ').strip()
+    return string
+
+
+# remove html tags
+def filterHtml(df):    
+    df1 = df.select_dtypes(include='string')
+    df1 = df1.applymap(lambda x: removeHtmlTags(str(x)))
+    df.update(df1)
+    return df
+
+
+# Main function to process csv files
+def process_csv_files():
+    # urls to obtain csv files
+    products_dir = "./data/PRODUCTS.csv"
+    prices_dir = "./data/PRICES-STOCK.csv"
+
+    # Create dataframes & filter into PRODUCTS.csv
+    products_df = pd.read_csv(products_dir, sep='|', index_col='SKU').convert_dtypes()
+    print(products_df['ITEM_DESCRIPTION'].head())
+    products_df = filterHtml(products_df)
+    print(products_df['ITEM_DESCRIPTION'].head())
+
+    products_df.to_csv('./out/PRODUCTS_mod.csv', header=True)
+
     '''
-    products_df.to_csv('./PRODUCTS.csv', index=False, header=True)
+    # Create dataframes from PRICES-STOCK.csv
+    prices_df = pd.read_csv(prices_dir, sep='|', index_col='SKU').convert_dtypes()
+    # include only elements in stock
+    prices_df[prices_df["STOCK"] > 0]
+    prices_df.to_csv('./out/PRICES-STOCK_mod.csv', header=True)
     '''
-    # put the dataframe into PRODUCTS.csv
-    prices_df = pd.read_csv(prices_url, sep='|')
-    print(prices_df.head())
-    print(products_df.head())
-    # prices_df.to_csv('./PRICES-STOCK.csv', index=False, header=True)
 
 
 if __name__ == "__main__":
+    checkFiles()
     process_csv_files()
