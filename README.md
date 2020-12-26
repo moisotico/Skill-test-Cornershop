c7fd64b78ecfb9b0d6fdbe11937ad2966dadc17e
moisotico <moiso.camposcr@gmail.com>
2020-12-26 12:22:36 -0600

    Working on requests. Access token ready!

--- README.md
+++ README.md
@@ -1,10 +1,91 @@
 # Cornershop's Skill Test for Integrations
 
+---
+
+
+
 ## About
 
-## Installing
+---
+
+The following is part of **Cornershop's backend skill integration test**. The requirements can be read at `instructions.md`
+
+The following instructions require [**Python 3** and **pip**](https://www.python.org/downloads/) installed. 
+
+
+
+## Downloading the repo
+
+----
+
+- Start by downloading this repo:
+
+  ```shell
+  $ cd <path_to_save>
+  $ git clone https://github.com/moisotico/Skill-test-Cornershop
+  ```
+
+- Create a file called `credentials.py`:
+
+  ```shell
+  $ cd Skill-test-Cornershop/
+  $ touch credentials.py
+  ```
+
+
+
+## Setting your environment
+
+----
+
+- Write the following in `credentials.py` and save:
+
+  ```python
+  GRAND_TYPE = "client_credentials"
+  CLIENT_ID = "mRkZGFjM"
+  CLIENT_SECRET = "ZGVmMjMz"
+  ```
+
+  ### Setting Anaconda/ Miniconda (option 1)
+
+- If you have **Anaconda** or **Miniconda** installed [(you can get it here)](https://docs.conda.io/projects/conda/en/latest/user-guide/install/), try the following steps:
+
+  ```shell
+  $ conda activate cornershop-test
+  $ pip install -r requirments.txt
+  ```
+
+  ### Setting Virtualenv (option 2)
+
+- If you prefer **Virtualenv**, try the following steps:
+
+  ```shell
+  $ pip install virtualenv
+  $ virtualenv cornershop-test
+  $ pip install -r requirments.txt
+  ```
+
+  
 
 ## Running
 
+---
+
+### Server
+
+- Please follow the instructions at `integration-skill-test-server-master/README.md` to run the Heroku app and API. **Once done, you can run the script**.
+
+### Script
+
+- We now run the  file `ingestion.py` :
+
+  ```shell
+  $ python3 ./integrations/richart_wholesale_club/ingestion.py
+  ```
+
+
+
 ## Notes
+
+---
 
--- integrations/richart_wholesale_club/ingestion.py
+++ integrations/richart_wholesale_club/ingestion.py
@@ -3,13 +3,16 @@ import pandas as pd
 import requests as rq
 import re
 import os
+from credentials import GRAND_TYPE, CLIENT_ID, CLIENT_SECRET, BASE_URL
 
-# using compile for a regular expression
-TAG_RE = re.compile(r'<[^>]+>')
+# Get credentials variables. TODO: check if this is safe
+grant_type = GRAND_TYPE
+cliend_id = CLIENT_ID
+client_secret = CLIENT_SECRET
+base_url = BASE_URL
+
 
 # function to check urls
-
-
 def getResponse(url):
     response = rq.get(url, allow_redirects=True)
     print("GET:", url, "Status Code:", response.status_code)
@@ -20,8 +23,15 @@ def getResponse(url):
         return -1
 
 
-# TODO: refactor
-def checkFiles(filename):
+def checkDir(dir):
+    try:
+        os.mkdir(dir)
+    except FileExistsError:
+        pass
+
+
+# Check if csv files exist, if not then download
+def checkFile(filename):
     filedir = './data/'
     filedir += str(filename)
     try:
@@ -29,10 +39,7 @@ def checkFiles(filename):
         f = open(filedir)
         return
     except FileNotFoundError:
-        try:
-            os.mkdir("./data")
-        except FileExistsError:
-            pass
+        checkDir("./data")
         # Get files from bucket
         print("File " + str(filedir) + " not accessible")
         print("Checking url...")
@@ -44,47 +51,99 @@ def checkFiles(filename):
         print("Finished downloading " + filedir + "!")
         pass
 
+
 # Function to remove HTML tags via a regex to match one or more characters
 #  inside a tag "< >""
-
-
 def removeHtmlTags(string):
-    string = TAG_RE.sub(' ', string)
+    string = re.sub('<[^>]+>',' ', string)
     string = string.replace('\\"', ' ').strip()
     return string
 
 
-# remove html tags
-def filterHtml(df):
-    df1 = df.select_dtypes(include='string')
-    df1 = df1.applymap(lambda x: removeHtmlTags(str(x)))
-    df.update(df1)
+# Edit Category to lowercase and join to "|" and Subcategory column string
+def editCategoryStr(row):
+    result = str(row['CATEGORY']).lower()
+    result += "|"
+    result += str(row['SUB_CATEGORY'])
+    return result
+
+
+# Join Category and Subcategory
+def joinCategories(df):
+    df['CATEGORY'] = df.apply(editCategoryStr, axis=1)
+    df = df.drop('SUB_CATEGORY', axis=1)
     return df
 
 
+# clean Products csv removing tags & strings, and
+# joining Categories & subcategoires
+def filterProducts(df):
+    df1 = df.select_dtypes(include='string')
+    df1 = df1.applymap(lambda x: removeHtmlTags(str(x)))
+    df.update(df1)
+    df = joinCategories(df)
+    return df
+
+
+# remove elements with stock =< 0
+def filterPrice(df):
+    df = df[df['STOCK'] > 0]
+    return df
+
+
+# Output CSVs
+def mergeDfsOut(df1, df2):
+    checkDir("./out")
+    df1.to_csv('./out/PRICES-STOCK_mod.csv', header=True)
+    df2.to_csv('./out/PRODUCTS_mod.csv', header=True)
+    joint_df = pd.merge(df1, df2,
+                        left_index=True, right_index=True, how='outer')
+    joint_df.to_csv('./out/JOINT.csv', header=True)
+    return joint_df
+
+
+# TODO: get the token
+def getCredentials():
+    url = f"{base_url}/oauth/token?client_id={cliend_id}" \
+        f"&client_secret={client_secret}" \
+        f"&grant_type={grant_type}"
+    payload = {}
+    headers = {}
+    response = rq.request("POST", url, headers=headers, data=payload)
+    r = response.json()
+    access_token = r.get('access_token')
+    return access_token
+
+
+# Requests
+def APIRequests():
+    cred_response = getCredentials()
+    print(cred_response)
+    pass
+
 # Main function to process csv files
+
+
 def process_csv_files():
     # urls to obtain csv files
     products_dir = "./data/PRODUCTS.csv"
     prices_dir = "./data/PRICES-STOCK.csv"
-
-    # Create dataframes & filter into PRODUCTS.csv
+    # Create dataframes & filter into PRODUCTS.csv, SKU as unique index
     products_df = pd.read_csv(products_dir, sep='|',
                               index_col='SKU').convert_dtypes()
-    products_df = filterHtml(products_df)
-    os.mkdir("./out")
+    products_df = filterProducts(products_df)
 
-    products_df.to_csv('./out/PRODUCTS_mod.csv', header=True)
-
-    # Create dataframes from PRICES-STOCK.csv
+    # Create dataframes from PRICES-STOCK.csv and filter to
+    # only elements in stock
     prices_df = pd.read_csv(prices_dir, sep='|',
                             index_col='SKU').convert_dtypes()
-    # include only elements in stock
-    prices_df[prices_df["STOCK"] > 0]
-    prices_df.to_csv('./out/PRICES-STOCK_mod.csv', header=True)
+    prices_df = filterPrice(prices_df)
+    merged_df = mergeDfsOut(prices_df, products_df)
+    APIRequests()
+    pass
 
 
 if __name__ == "__main__":
-    checkFiles("PRODUCTS.csv")
-    checkFiles("PRICES-STOCK.csv")
+    checkFile("PRODUCTS.csv")
+    checkFile("PRICES-STOCK.csv")
     process_csv_files()
