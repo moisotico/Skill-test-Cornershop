2c2198bd16ce27d2c2c24e23e66f0e76d170fa1b
moisotico <moiso.camposcr@gmail.com>
2020-12-26 17:42:41 -0600

    deleteMerchant working, updateMerchant pending

--- README.md
+++ README.md
@@ -1,22 +1,14 @@
 # Cornershop's Skill Test for Integrations
 
----
-
 
 
 ## About
 
----
-
-The following is part of **Cornershop's backend skill integration test**. The requirements can be read at `instructions.md`
-
-The following instructions require [**Python 3** and **pip**](https://www.python.org/downloads/) installed. 
+The following is part of **Cornershop's backend skill integration test**. The requirements can be read at `instructions.md`. The following instructions require [**Python 3** and **pip**](https://www.python.org/downloads/) installed. 
 
 
 
-## Downloading the repo
-
-----
+## Downloading 
 
 - Start by downloading this repo:
 
@@ -25,18 +17,18 @@ The following instructions require [**Python 3** and **pip**](https://www.python
   $ git clone https://github.com/moisotico/Skill-test-Cornershop
   ```
 
-- Create a file called `credentials.py`:
-
-  ```shell
-  $ cd Skill-test-Cornershop/
-  $ touch credentials.py
-  ```
-
-
+  
 
 ## Setting your environment
 
-----
+### API Credentials
+
+- Create a file called `credentials.py`:
+
+  ```shell
+  $ cd Skill-test-Cornershop/integrations/richart_wholesale_club/
+  $ touch credentials.py
+  ```
 
 - Write the following in `credentials.py` and save:
 
@@ -44,9 +36,10 @@ The following instructions require [**Python 3** and **pip**](https://www.python
   GRAND_TYPE = "client_credentials"
   CLIENT_ID = "mRkZGFjM"
   CLIENT_SECRET = "ZGVmMjMz"
+  BASE_URL = "https://integration-skill-test-server.herokuapp.com/"
   ```
 
-  ### Setting Anaconda/ Miniconda (option 1)
+### Option 1: Setting Anaconda/ Miniconda 
 
 - If you have **Anaconda** or **Miniconda** installed [(you can get it here)](https://docs.conda.io/projects/conda/en/latest/user-guide/install/), try the following steps:
 
@@ -55,7 +48,7 @@ The following instructions require [**Python 3** and **pip**](https://www.python
   $ pip install -r requirments.txt
   ```
 
-  ### Setting Virtualenv (option 2)
+### Option 2: Setting Virtualenv
 
 - If you prefer **Virtualenv**, try the following steps:
 
@@ -69,15 +62,13 @@ The following instructions require [**Python 3** and **pip**](https://www.python
 
 ## Running
 
----
-
 ### Server
 
 - Please follow the instructions at `integration-skill-test-server-master/README.md` to run the Heroku app and API. **Once done, you can run the script**.
 
 ### Script
 
-- We now run the  file `ingestion.py` :
+- Run the  file `ingestion.py` :
 
   ```shell
   $ python3 ./integrations/richart_wholesale_club/ingestion.py
@@ -87,5 +78,5 @@ The following instructions require [**Python 3** and **pip**](https://www.python
 
 ## Notes
 
----
+
 
--- integrations/richart_wholesale_club/ingestion.py
+++ integrations/richart_wholesale_club/ingestion.py
@@ -23,6 +23,7 @@ def getResponse(url):
         return -1
 
 
+# check if directoriy exists, else create it
 def checkDir(dir):
     try:
         os.mkdir(dir)
@@ -48,30 +49,39 @@ def checkFile(filename):
         r = getResponse(url)
         print("Downloading " + filename)
         open(filedir, 'wb').write(r.content)
-        print("Finished downloading " + filedir + "!")
+        print("Finished downloading to" + filedir)
         pass
 
 
 # Function to remove HTML tags via a regex to match one or more characters
 #  inside a tag "< >""
 def removeHtmlTags(string):
-    string = re.sub('<[^>]+>',' ', string)
+    string = re.sub('<[^>]+>', ' ', string)
     string = string.replace('\\"', ' ').strip()
     return string
 
 
-# Edit Category to lowercase and join to "|" and Subcategory column string
+# Edit Category to lowercase and join to "|" and Subcategories column string
 def editCategoryStr(row):
-    result = str(row['CATEGORY']).lower()
+    result = str(row['CATEGORY'])
     result += "|"
     result += str(row['SUB_CATEGORY'])
-    return result
+    result += "|"
+    result += str(row['SUB_SUB_CATEGORY'])
+    return result.lower()
+
+# TODO: Get package from product
+
+
+def getPackage(string):
+    return string
 
 
 # Join Category and Subcategory
 def joinCategories(df):
     df['CATEGORY'] = df.apply(editCategoryStr, axis=1)
     df = df.drop('SUB_CATEGORY', axis=1)
+    df = df.drop('SUB_SUB_CATEGORY', axis=1)
     return df
 
 
@@ -94,15 +104,23 @@ def filterPrice(df):
 # Output CSVs
 def mergeDfsOut(df1, df2):
     checkDir("./out")
-    df1.to_csv('./out/PRICES-STOCK_mod.csv', header=True)
-    df2.to_csv('./out/PRODUCTS_mod.csv', header=True)
+    df1.to_csv('./out/PRODUCTS_mod.csv', header=True)
+    df2.to_csv('./out/PRICES-STOCK_mod.csv', header=True)
     joint_df = pd.merge(df1, df2,
                         left_index=True, right_index=True, how='outer')
     joint_df.to_csv('./out/JOINT.csv', header=True)
     return joint_df
 
 
-# TODO: get the token
+# Search for a specific merchant in a JSON formatted response
+def searchMerchant(merchant, items):
+    for keyvalues in items['merchants']:
+        if keyvalues['name'].lower() == merchant.lower():
+            return keyvalues['id']
+    return None
+
+
+# get a valid access token
 def getCredentials():
     url = f"{base_url}/oauth/token?client_id={cliend_id}" \
         f"&client_secret={client_secret}" \
@@ -111,19 +129,61 @@ def getCredentials():
     headers = {}
     response = rq.request("POST", url, headers=headers, data=payload)
     r = response.json()
-    access_token = r.get('access_token')
-    return access_token
+    return r.get('access_token')
+
+
+# get Richart's Wholesale Club ID
+def getMerchants(token, name):
+    url = f"{base_url}/api/merchants"
+    payload = {}
+    headers = {
+        'token': f'Bearer {token}'
+    }
+    response = rq.request("GET", url, headers=headers, data=payload)
+    r = response.json()
+    merchant_id = searchMerchant(name, r)
+    return merchant_id, r
+
+
+# update is_active field to true
+def updateMerchant(token, merchant_id, merchant):
+    url = f"{base_url}/api/merchants/{merchant_id}"
+    payload = f"{{\n    \"can_be_deleted\": false,\n    \"can_be_updated\": true,\n    \"id\": \"{merchant_id}\",\n    \"is_active\": true,\n    \"name\": \"{merchant}\"\n}}"
+    headers = {
+        'token': f'Bearer {token}'
+    }
+    response = rq.request("PUT", url, headers=headers, data=payload)
+    print(response)
+    pass
+
+
+# delete
+def deleteMerchant(token, merchant, response):
+    merchant_id = searchMerchant(merchant, response)
+    print(merchant, merchant_id)
+    url = f"{base_url}/api/merchants/{merchant_id}"
+    payload = {}
+    headers = {
+        'token': f'Bearer {token}'
+    }
+    response = rq.request("DELETE", url, headers=headers, data=payload)
+    print(response)
+    pass
 
 
 # Requests
 def APIRequests():
-    cred_response = getCredentials()
-    print(cred_response)
+    merchant = "Richard\'s"
+    # valid_token = getCredentials()
+    # TODO: remove this declaration of valid_token
+    valid_token = "JYY3h3AAcTm3YmcMJAAM"
+    merchant_id, r = getMerchants(valid_token, merchant)
+    #updateMerchant(valid_token, merchant_id, merchant)
+    deleteMerchant(valid_token, "Beauty", r)
     pass
 
+
 # Main function to process csv files
-
-
 def process_csv_files():
     # urls to obtain csv files
     products_dir = "./data/PRODUCTS.csv"
@@ -138,7 +198,7 @@ def process_csv_files():
     prices_df = pd.read_csv(prices_dir, sep='|',
                             index_col='SKU').convert_dtypes()
     prices_df = filterPrice(prices_df)
-    merged_df = mergeDfsOut(prices_df, products_df)
+    products_df = mergeDfsOut(products_df, prices_df)
     APIRequests()
     pass
 
